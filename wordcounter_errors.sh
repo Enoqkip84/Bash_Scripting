3407492d749abad37c6dbb80fafb0037e8a859b0
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-05-24 02:31:39 +0800

    files

--- wordcounter_errors.sh
+++ wordcounter_errors.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-lcnt=1
+lcnt=0
 
-while read -r line; do
+while read -r line; 
+do
 	(( lcnt++ ))
-	wcnt=`echo $line | wc -c`
+	wcnt=`echo "$line" | wc -w`
 	echo "Line $lcnt contains $wcnt words"
 done < sentences.txt
 
